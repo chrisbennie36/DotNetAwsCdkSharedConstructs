5e10931fa2799d54e0e21ae71eff820462aac3b9
unknown <chrisbennie0036@gmail.com>
2025-01-01 12:22:19 +0100

    Added shared Elastic Beanstalk class which handles the setting up of an ElasticBeanstalk instance on AWS with the given config values or the defaults if not specified

---
+++ AwsCdkSharedConstructs/ElasticBeanstalk.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using Amazon.CDK;
+using Amazon.CDK.AWS.EC2;
+using Amazon.CDK.AWS.ElasticBeanstalk;
+using Amazon.CDK.AWS.IAM;
+using Amazon.CDK.AWS.S3.Assets;
+using Constructs;
+
+namespace AwsCdkSharedConstructs;
+
+public class ElasticBeanstalk()
+{
+    public CfnApplication ElasticBeanstalkApplication { get; set; }
+
+    public void Build(Construct scope, ElasticBeanstalkProps props)
+    {
+        IRole role= Role.FromRoleName(scope, $"{props.ApplicationId}-eb-app-role", props.ExistingRoleName);
+
+        var instanceProfile = new InstanceProfile(scope, $"{props.ApplicationId}-eb-instance-profile", new InstanceProfileProps
+        {
+            Role = role,
+            InstanceProfileName = props.InstanceProfileName
+        });
+
+        _ = new CfnOutput(scope, $"{props.ApplicationId}-eb-iam-role", new CfnOutputProps 
+        {
+            Value = role.RoleArn
+        });
+
+        var archive = new Asset(scope, $"{props.ApplicationId}-app-zip-location", new AssetProps
+        {
+            Path = props.AssetPath
+        });
+
+        ElasticBeanstalkApplication = new Amazon.CDK.AWS.ElasticBeanstalk.CfnApplication(scope, $"{props.ApplicationId}-elb-app", new CfnApplicationProps 
+        {
+            ApplicationName = props.ApplicationName
+        });
+
+        CfnApplicationVersion applicationVersion = new Amazon.CDK.AWS.ElasticBeanstalk.CfnApplicationVersion(scope, $"{props.ApplicationId}-elb-app-version", new CfnApplicationVersionProps
+        {
+            ApplicationName = props.ApplicationName,
+            SourceBundle = new Amazon.CDK.AWS.ElasticBeanstalk.CfnApplicationVersion.SourceBundleProperty
+            {
+                S3Bucket = archive.S3BucketName,
+                S3Key = archive.S3ObjectKey
+            }
+        });
+
+        CfnEnvironment elasticBeanstalkEnvironment = new CfnEnvironment(scope, $"{props.ApplicationId}-elb-environment", new CfnEnvironmentProps
+        {
+            ApplicationName = props.ApplicationName,
+            OptionSettings = new CfnEnvironment.OptionSettingProperty[] 
+            {
+                new CfnEnvironment.OptionSettingProperty { Namespace = "aws:autoscaling:launchconfiguration", OptionName = "IamInstanceProfile", Value = instanceProfile.InstanceProfileArn },
+                new CfnEnvironment.OptionSettingProperty { Namespace = "aws:autoscaling:launchconfiguration", OptionName = "RootVolumeType", Value = props.RootVolumeType },
+                new CfnEnvironment.OptionSettingProperty { Namespace = "aws:autoscaling:asg", OptionName = "MaxSize", Value = props.AutoScalingMaxSize },
+                new CfnEnvironment.OptionSettingProperty { Namespace = "aws:autoscaling:asg", OptionName = "MinSize", Value = props.AutoScalingMinSize },
+            },
+            EnvironmentName = props.EnvironmentName,
+            SolutionStackName = props.SolutionStackName,
+            VersionLabel = applicationVersion.Ref   //Critical apparently
+        });
+
+        elasticBeanstalkEnvironment.AddDependency(ElasticBeanstalkApplication);
+        applicationVersion.AddDependency(ElasticBeanstalkApplication);
+    }
+}
---
+++ AwsCdkSharedConstructs/ElasticBeanstalkProps.cs
@@ -0,0 +1,17 @@
+using Amazon.CDK;
+
+namespace AwsCdkSharedConstructs;
+
+public class ElasticBeanstalkProps : StackProps
+{
+    public string ExistingRoleName { get; set; } = "aws-elasticbeanstalk-ec2-role";
+    public string InstanceProfileName { get; set; } = string.Empty;
+    public string AssetPath { get; set; } = "../application.zip";
+    public string ApplicationName { get; set; } = string.Empty;
+    public string ApplicationId { get; set; } = string.Empty;
+    public string RootVolumeType { get; set; } = "gp3";
+    public string AutoScalingMaxSize { get; set; } = "1";
+    public string AutoScalingMinSize { get; set; } = "1";
+    public string EnvironmentName { get; set; } = "Projects"; //Must be > 4 chars
+    public string SolutionStackName { get; set; } = "64bit Amazon Linux 2023 v3.2.1 running .NET 8";
+}
